688b0fb9ca5b67a0c57f52c70813d931e62f6fb8
Fenysk <minefenysk@gmail.com>
2023-05-29 12:57:28 +0200

    :twisted_rightwards_arrows: Merge API calls from View to dedicated files + fix account creation

--- src/views/User/ConnexionView.vue
+++ src/views/User/ConnexionView.vue
@@ -1,42 +1,89 @@
 <template>
-    <div class="
-    connexion
-    xl:px-64 lg:px-32 md:px-16 sm:px-8 px-4 ease-in-out duration-300 py-16
-    ">
-        <h2 class="text-center">Connexion</h2>
-        <form @submit.prevent="login" class="mt-6">
+    <div
+        class="connexion xl:px-64 lg:px-32 md:px-16 sm:px-8 px-4 ease-in-out duration-300 py-16"
+    >
+        <h2 class="text-center">
+            {{ isSignUp ? "Inscription" : "Connexion" }}
+        </h2>
+        <form @submit.prevent="isSignUp ? signup() : login()">
+            <input
+                type="username"
+                id="username"
+                placeholder="Pseudo"
+                v-model="user.username"
+                required
+                autofocus
+            />
 
-            <input type="username" id="username" placeholder="Pseudo" v-model="user.username" required autofocus/>
+            <input
+                v-if="isSignUp"
+                type="firstName"
+                id="firstName"
+                placeholder="Prénom"
+                v-model="user.firstName"
+                required
+            />
 
-            <input v-if="isSignUp" type="firstName" id="firstName" placeholder="Prénom" v-model="user.firstName" required />
+            <input
+                v-if="isSignUp"
+                type="lastName"
+                id="lastName"
+                placeholder="Nom"
+                v-model="user.lastName"
+                required
+            />
 
-            <input v-if="isSignUp" type="lastName" id="lastName" placeholder="Nom" v-model="user.lastName" required />
+            <input
+                v-if="isSignUp"
+                type="text"
+                id="email"
+                placeholder="Email"
+                v-model="user.email"
+                required
+            />
 
-            <input v-if="isSignUp" type="text" id="email" placeholder="Email" v-model="user.email" required />
+            <input
+                type="password"
+                id="password"
+                placeholder="Mot de passe"
+                v-model="user.password"
+                required
+            />
 
-            <input type="password" id="password" placeholder="Mot de passe" v-model="user.password" required />
-
-            <input v-if="isSignUp" type="date" id="birthDate" placeholder="Date de naissance" v-model="user.birthDate" required />
-
+            <input
+                v-if="isSignUp"
+                type="date"
+                id="birthDate"
+                placeholder="Date de naissance"
+                v-model="user.birthDate"
+                required
+            />
 
             <div class="flex justify-center mt-4">
-                <Button class="cursor-pointer"
+                <Button
+                    class="cursor-pointer"
                     :text="isSignUp ? 'S\'inscrire' : 'Se connecter'"
                     @click="isSignUp ? signup() : login()"
                 />
-                <Button class="cursor-pointer"
-                    :text="isSignUp ? 'J\ai déjà un compte' : 'Je n\'ai pas de compte'"
-                    @click="switchMode" 
+                <Button
+                    class="cursor-pointer"
+                    :text="
+                        isSignUp
+                            ? 'J\ai déjà un compte'
+                            : 'Je n\'ai pas de compte'
+                    "
+                    @click="switchMode"
                     secondary
                 />
             </div>
-
         </form>
-    </div>    
+    </div>
 </template>
 
 <script>
 import axios from "axios";
+import { postLogin } from "@/api/Auth/postLogin.js";
+import { postUser } from "@/api/Auth/postUser.js";
 
 import Button from "../../components/elements/Button.vue";
 
@@ -48,95 +95,78 @@ export default {
     data() {
         return {
             user: {
-                username: '',
-                firstName: '',
-                lastName: '',
-                email: '',
-                password: '',
-                birthDate: '',
-                birthDateFormatted: '',
+                username: "",
+                firstName: "",
+                lastName: "",
+                email: "",
+                password: "",
+                birthDate: "",
+                birthDateFormatted: "",
             },
-            token: '',
+            token: "",
 
             isSignUp: false,
         };
     },
 
     watch: {
-        'user.birthDate': function (val) {
+        "user.birthDate": function (val) {
             console.log(val);
-            this.user.birthDateFormatted = val + 'T12:00:00.000Z';
-        }
+            this.user.birthDateFormatted = val + "T12:00:00.000Z";
+        },
     },
 
     methods: {
-
         switchMode() {
             this.isSignUp = !this.isSignUp;
             console.log(this.isSignUp);
         },
 
-        async login() {
-            const apiUrl = 'https://digiquest-back.herokuapp.com';
-            axios.post(`${apiUrl}/login`,
-                {
-                    "username": this.user.username,
-                    "password": this.user.password,
-                },
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                    }
-                }
-            ).then(response => {
-                    console.log(response.data);
-                    localStorage.setItem('token', response.data.token);
-                    this.token = response.data.token;
+        login() {
+            postLogin(this.user.username, this.user.password)
+                .then((token) => {
+                    localStorage.setItem("token", token);
+                    this.token = token;
                     this.$nextTick(() => {
                         window.location.reload();
-                    })
+                    });
                 })
-                .catch(error => {
+                .catch((error) => {
                     console.log(error);
-                })
+                });
         },
 
-        async signup() {
-            const apiUrl = 'https://digiquest-back.herokuapp.com';
-            axios.post(`${apiUrl}/user`,
-                {
-                    "username": this.user.username,
-                    "password": this.user.password,
-                    "email": this.user.email,
-                    "firstName": this.user.firstName,
-                    "lastName": this.user.lastName,
-                    "birthDate": this.user.birthDateFormatted,
-                },
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
+        signup() {
+            postUser(
+                this.user.username,
+                this.user.password,
+                this.user.email,
+                this.user.firstName,
+                this.user.lastName,
+                this.user.birthDateFormatted
+            )
+                .then((message) => {
+                    console.log(message);
+                    if (message === "Creation complete") {
+                        this.login();
                     }
-                }
-            ).then(response => {
-                    console.log(response.data);
-                    localStorage.setItem('token', response.token);
-                    this.token = response.data.token;
-                    this.$nextTick(() => {
-                        window.location.reload();
-                    })
                 })
-                .catch(error => {
+                .catch((error) => {
                     console.log(error);
-                })
+                });
         },
     },
 
     mounted() {
-        if (localStorage.getItem('token')) {
-            this.$router.push('/my-account');
+        try {
+            if (localStorage.getItem("token")) {
+                this.$router.push("/my-account");
+            }
+        } catch (error) {
+            this.$router.push("/connexion");
         }
-    }
-}
+    },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -166,5 +196,4 @@ input {
         opacity: 0.5;
     }
 }
-
 </style>
